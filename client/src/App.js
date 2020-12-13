9e642be2cb1e6ab1b6849afd1a4e9315eb21a1bf
Khaled <kayloody@gmail.com>
2020-12-13 16:05:38 -0700

    Setup routes for user pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  //useRouteMatch,
+} from 'react-router-dom';
 
 import Swftli from './components/Swftli/Swftli.js';
 
 const App = () => {
-  return <Swftli />;
+  return (
+    <Router>
+      <Switch>
+        <Route path='/:uid' children={<Swftli />} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
