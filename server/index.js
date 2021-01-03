cc8ce0b38284aa5551e6f78861ab7d5277170e4f
Khaled <kayloody@gmail.com>
2021-01-02 20:02:11 -0700

    Starting work for deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import path from 'path';
 
 import passport from 'passport';
 import passportSetup from './config/passport-setup.js';
@@ -27,6 +28,11 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
+
+app.use((req, res, next) => {
+  console.log(`Request_Endpoint: ${req.method} ${req.url}`);
+  next();
+});
 
 app.use(
   cookieSession({
