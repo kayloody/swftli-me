e25b3a51080156b84a01e6d2fec981e45ff6e987
Khaled <kayloody@gmail.com>
2021-01-02 19:03:49 -0700

    Updated keys

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -2,6 +2,6 @@ import cloudinary from 'cloudinary';
 
 export default cloudinary.config({
   cloud_name: 'swftli-me',
-  api_key: '297792667697841',
-  api_secret: 'Y9MVqbeiRK80qokLW9tMnu6SAUA',
+  api_key: '593369536866941	',
+  api_secret: 'MWRLfAULHmoSt5Fm4STuG4l25NA',
 });
