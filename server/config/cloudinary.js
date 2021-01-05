e0ff3ba7d5e18ca3c72707d52bf43a8b241a6a79
Khaled <65138088+kayloody@users.noreply.github.com>
2021-01-05 01:53:18 -0700

    Delete cloudinary.js

--- server/config/cloudinary.js
+++
@@ -1,7 +0,0 @@
-import cloudinary from 'cloudinary';
-
-export default cloudinary.config({
-  cloud_name: 'swftli-me',
-  api_key: '618554389982445',
-  api_secret: '1oe1KFGIEAnO6NkAewcljpkI2Yk',
-});
